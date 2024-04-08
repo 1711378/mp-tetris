a8f0dde469edc3c8bf5da4e3f779f81a67b67572
1711378 <74317636+1711378@users.noreply.github.com>
2024-04-08 23:20:30 +0200

    Esqueleto de la clase Joc

--- Joc.h
+++ Joc.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef TAULER.H
+#define TAULER.H
+
+#include "Figura.h"
+
+class Joc {
+public:
+	Joc();
+	void inicialitza();
+	void giraFigura();
+	void mouFigura();
+	void baixaFigura();
+	void escriuTauler();
+private:
+	Figura m_figuraActual;
+};
+
+#endif
